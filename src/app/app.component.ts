05e9e2e0faee595597fd70aef38d494dee0c7347
Alexander Ivanov <pravbeseda@yandex.ru>
2023-12-05 08:06:30 +0600

    added spectator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {RouterModule} from "@angular/router";
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'drevo-root',
+  standalone: true,
+    imports: [
+      RouterModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'drevo';
+
 }
