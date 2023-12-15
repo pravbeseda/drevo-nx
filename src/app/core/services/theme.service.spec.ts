8079078188e6eb807be5dcab06b4371607ff0b95
Alexander Ivanov <pravbeseda@yandex.ru>
2023-12-15 07:57:31 +0600

    theme switcher final (fix)

--- src/app/core/services/theme.service.spec.ts
+++ src/app/core/services/theme.service.spec.ts
@@ -1,4 +1,4 @@
-import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator/jest';
 import { ThemeService } from './theme.service';
 import { OverlayContainer } from '@angular/cdk/overlay';
 import { THEMES } from '../../shared/consts/themes';
