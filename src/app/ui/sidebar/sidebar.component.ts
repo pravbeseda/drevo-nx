1d1f8c985040956c9f47f5dade0fe588307422c6
Alexander Ivanov <pravbeseda@yandex.ru>
2023-12-18 08:10:04 +0600

    Left sidebar works well

--- src/app/ui/sidebar/sidebar.component.ts
+++ src/app/ui/sidebar/sidebar.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { LayoutService } from '../../core/services/layout.service';
 import { MatListModule } from '@angular/material/list';
@@ -14,39 +9,39 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { BehaviorSubject } from 'rxjs';
 
 @UntilDestroy()
 @Component({
-  selector: 'drevo-sidebar',
-  standalone: true,
-  imports: [
-    MatSidenavModule,
-    MatListModule,
-    RouterLink,
-    AsyncPipe,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    NavbarComponent,
-  ],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'drevo-sidebar',
+    standalone: true,
+    imports: [
+        MatSidenavModule,
+        MatListModule,
+        RouterLink,
+        AsyncPipe,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        NavbarComponent,
+    ],
+    templateUrl: './sidebar.component.html',
+    styleUrl: './sidebar.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent implements OnInit {
-  public readonly sidebarOpened$ = this.layoutService.sidebarOpened$;
-  public mobileQuery: MediaQueryList | undefined;
-  constructor(
-    private readonly layoutService: LayoutService,
-    private readonly cdRef: ChangeDetectorRef
-  ) {}
+    public readonly sidebarOpened$ = this.layoutService.sidebarOpened$;
 
-  ngOnInit(): void {
-    this.layoutService.mobileQuery$
-      .pipe(untilDestroyed(this))
-      .subscribe(mobileQuery => {
-        this.mobileQuery = mobileQuery;
-        this.cdRef.detectChanges();
-      });
-  }
+    private readonly _mobileModeSubject = new BehaviorSubject<boolean>(true);
+    public readonly mobileMode$ = this._mobileModeSubject.asObservable();
+
+    constructor(private readonly layoutService: LayoutService) {}
+
+    ngOnInit(): void {
+        this.layoutService.mobileQuery$
+            .pipe(untilDestroyed(this))
+            .subscribe(mobileQuery => {
+                this._mobileModeSubject.next(mobileQuery.matches);
+            });
+    }
 }
