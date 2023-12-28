0cea6aa6bf53c704b292f6aded15affb36fcf506
Alexander Ivanov <pravbeseda@yandex.ru>
2023-12-28 06:51:24 +0600

    Auto-opened sidebar

--- src/app/ui/layout/layout.component.ts
+++ src/app/ui/layout/layout.component.ts
@@ -3,12 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ThemeService } from '../../core/services/theme.service';
 import { AsyncPipe } from '@angular/common';
-import { LayoutService } from '../../core/services/layout.service';
+import { LayoutService, ScreenSize } from '../../core/services/layout.service';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLink } from '@angular/router';
 import { SidebarLeftContentComponent } from '../sidebar-left-content/sidebar-left-content.component';
 import { SidebarRightContentComponent } from '../sidebar-right-content/sidebar-right-content.component';
+import { map } from 'rxjs';
 
 const MATERIAL_IMPORTS = [MatButtonModule];
 
@@ -33,6 +34,9 @@ const MATERIAL_IMPORTS = [MatButtonModule];
 export class LayoutComponent {
     public readonly theme$ = this.themeService.currentTheme$;
     public readonly isMobile$ = this.layoutService.isMobile$;
+    public readonly isLargeScreen$ = this.layoutService.currentScreenSize$.pipe(
+        map(size => size >= ScreenSize.Large)
+    );
 
     constructor(
         private readonly themeService: ThemeService,
