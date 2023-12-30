e128eaf4277c317fc03ee2e4f1e10ba86fa34b85
pravbeseda <pravbeseda@users.noreply.github.com>
2023-12-30 05:15:24 +0000

    deploy: d3e38593d003a66b107a6a73ca0fcfa4f078682b

--- chunk-V4PSMC7H.js
+++
@@ -1,1 +0,0 @@
-import{Ab as r,Fb as c,da as o,nb as i,ob as a}from"./chunk-DVW24HQX.js";var p=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["drevo-main"]],standalone:!0,features:[c],decls:2,vars:0,template:function(n,m){n&1&&(i(0,"p"),r(1,"main works!"),a())},changeDetection:0});let t=e;return t})();export{p as MainComponent};
